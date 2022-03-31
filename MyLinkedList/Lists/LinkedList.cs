4fa9f49345c9e6613860205fa2d6d0a53700e461
Maxim Kuzmenko <mway2914@yandex.ru>
2022-03-31 12:46:44 +0300

    add LinkedList

---
+++ MyLinkedList/Lists/LinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class LinkedList
+    {
+        private Node _root;
+
+        private Node _tail;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index<0 || index>= Length)
+                {
+                    throw new IndexOutOfRangeException("This index doesn't exist");
+                }
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                return crnt.Value;
+            }
+
+            set
+            {
+
+                if (index < 0 || index >= Length)
+                {
+                    throw new IndexOutOfRangeException("This index doesn't exist");
+                }
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                crnt.Value=value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+                Node crnt = _root;
+
+                while (crnt != null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+
+                return count;
+            }
+            private set
+            {
+            }
+        }
+
+        public LinkedList()
+        {
+            _root = null;
+
+            _tail = null;
+        }
+
+        public LinkedList(int value)
+        {
+            _root = new Node(value);
+            _tail = _root;
+        }
+
+        public LinkedList(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                _root = null;
+                _tail = null;
+            }
+            else
+            {
+                _root = new Node(array[0]);
+                Node crnt = _root;
+                for (int i=1; i< array.Length; i++)
+                {
+                    crnt.Next = new Node(array[i]);
+                    crnt = crnt.Next;
+                }
+                _tail = crnt;
+            }
+        }
+
+    }
+}
