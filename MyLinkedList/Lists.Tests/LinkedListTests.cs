d04378075fc290de493cca2944c70916491c6e6c
Maxim Kuzmenko <mway2914@yandex.ru>
2022-03-31 20:54:50 +0300

    LinkedListTests inital commit

---
+++ MyLinkedList/Lists.Tests/LinkedListTests.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+using System.Collections;
+//using ArrayList.Tests.ArrayListTestsSources;
+//using ArrayList.Tests.ArrayListNegativeTestsSources;
+
+namespace Lists.Tests
+{
+    public class LinkedListTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
